8e31bfe00c0a16452ac68da388acea3730ed1d12
Mitchell Stack <mitchellrstack@gmail.com>
2017-12-06 12:59:27 -0500

    Add Day Six Solution

--- day-06/index.js
+++ day-06/index.js
@@ -1,44 +1,137 @@
 /**
- * Day 6: Memory Relocation
+ * Day 6: Memory Relocation Part One
  */
 const distributeMemory = memoryBank => {
-  let state = []
+  // For saving each state of the bank to check for inifinite loops
+  let states = []
+  // Make a copy of the input bank for modifying
+  let bank = memoryBank.slice()
+  // Convert to string for easy state comparison
+  let bankString = bank.join(', ')
+  // Push the string to the states for later checking of infinite loops
+  states.push(bankString)
+  // Switch to true if infinite loop detected, cycle will stop
+  let isLoop = false
+  // Count the number of cycles it took to redistribute the banks
+  let redistributions = 0
 
-  // Have to make copy of array to modify and push to state
-  let bank = memoryBank.join(', ') // Possibly set state as string for easier comparison
-  state.push(bank)
-  console.log(state)
+  // Start cycle
+  while (!isLoop) {
+    // Make new copy of bank
+    let newBank = bank.slice()
+    
+    // Select the bank with most blocks for redistribution
+    let largestSum = newBank.reduce((a, b) => Math.max(a, b))
+    let redisIndex = newBank.indexOf(largestSum)
 
-  let newBank = bank.slice()
-  newBank[0] += 10
+    // Store the blocks temporarily (Maybe in the storage closet)
+    let blocks = newBank[redisIndex]
+    // Clear the fake blocks from the bank
+    newBank[redisIndex] = 0
 
-  state.push(newBank)
+    // Distribute the blocks starting with block after the redisIndex
+    while (blocks > 0) {
+      redisIndex++
+      if (redisIndex > newBank.length - 1) {
+        redisIndex = 0
+      }
 
-  let newBank2 = newBank.slice()
-  newBank2[0] -= 10
+      // Take a block from the storage (closet) and put in the selected bank
+      blocks--
+      newBank[redisIndex]++
+    }
 
-  if (state.indexOf(newBank2) !== -1) {
-    console.log('INIFINITY')
-  } else {
-    state.push(newBank2)
+    let newBankString = newBank.join(', ')
+    // If the string is in the state array, it is an infinite loop
+    if (states.indexOf(newBankString) !== -1) {
+      isLoop = true
+    }
+    // Push the string to the states since it it not already there
+    states.push(newBankString)
+
+    // Make global bank a copy of modified for next cycle
+    bank = newBank.slice()
+
+    // Increment the number of redistributions
+    redistributions++
   }
 
-  
-
-  console.log(state)
+  return redistributions
 }
 
-module.exports = distributeMemory
-
-/*
-
-Cycle
-- finds the memory bank with the most blocks (ties won by the lowest-numbered memory bank)
-- redistributes those blocks among the banks.
-- removes all of the blocks from the selected bank
-- moves to the next (by index) memory bank and inserts one of the blocks.
-
-continues doing this until it runs out of blocks; if it reaches the last memory bank, it wraps around to the first one.
+/**
+ * !! NOT CURRENTLY WORKING NEED TO FIX !!
+ * Day 6: Memory Relocation Part Two
  */
+const distributeMemory2 = (memoryBank) => {
+  // For saving each state of the bank to check for inifinite loops
+  let states = []
+  // Make a copy of the input bank for modifying
+  let bank = memoryBank.slice()
+  // Convert to string for easy state comparison
+  let bankString = bank.join(', ')
+  // Push the string to the states for later checking of infinite loops
+  states.push(bankString)
+  // Switch to true if infinite loop detected, cycle will stop
+  let isLoop = false
+  let loopFlag = ''
+  // Count the number of cycles it took to redistribute the banks
+  let redistributions = 0
 
-distributeMemory([0, 2, 7, 0])
+  // Start cycle
+  while (!isLoop) {
+    // Make new copy of bank
+    let newBank = bank.slice()
+    
+    // Select the bank with most blocks for redistribution
+    let largestSum = newBank.reduce((a, b) => Math.max(a, b))
+    let redisIndex = newBank.indexOf(largestSum)
+
+    // Store the blocks temporarily (Maybe in the storage closet)
+    let blocks = newBank[redisIndex]
+    // Clear the fake blocks from the bank
+    newBank[redisIndex] = 0
+
+    // Distribute the blocks starting with block after the redisIndex
+    while (blocks > 0) {
+      redisIndex++
+      if (redisIndex > newBank.length - 1) {
+        redisIndex = 0
+      }
+
+      // Take a block from the storage (closet) and put in the selected bank
+      blocks--
+      newBank[redisIndex]++
+    }
+
+    let newBankString = newBank.join(', ')
+    // If the string is in the state array, it is an infinite loop
+    console.log(states.length)
+    if (states.indexOf(newBankString) !== -1) {
+      isLoop = true
+      loopFlag = newBankString
+    }
+    // Push the string to the states since it it not already there
+    states.push(newBankString)
+
+    // Make global bank a copy of modified for next cycle
+    bank = newBank.slice()
+
+    // Increment the number of redistributions
+    redistributions++
+  }
+
+  return { r: redistributions, state: loopFlag }
+}
+
+module.exports = { distributeMemory, distributeMemory2 }
+
+// For the Advent Code Challenge answer
+const challengeAnswer1 = distributeMemory([11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11])
+console.log(`Day 06 Challenge Part 1 Answer: ${challengeAnswer1}`)
+
+// For the Advent Code Challenge answer
+const challengeAnswer2loop1 = distributeMemory2([11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11])
+const challengeAnswer2loop2 = distributeMemory2(challengeAnswer2loop1.state.split(', '))
+console.log(`Day 06 Challenge Part 2 Answer:\n`, challengeAnswer2loop1)
+console.log(`Day 06 Challenge Part 2 Answer:\n`, challengeAnswer2loop2)
